a370f31155a9d16c353e8546466412cac0ce2c33
hellohellohell012321 <hhellohellohell012321@gmail.com>
2025-12-22 00:30:40 -0800

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ import Navbar from "@/components/Shared/Navbar";
 export const metadata: Metadata = {
   title: "MCMarket",
   description: "A website to simplify selling and buying Minecraft accounts.",
+  openGraph: {
+    title: "MCMarket",
+    description: "A website to simplify selling and buying Minecraft accounts.",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
