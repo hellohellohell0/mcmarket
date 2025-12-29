106435a43574273b862568316de603c659ff18d6
hellohellohell012321 <hhellohellohell012321@gmail.com>
2025-12-29 00:21:24 -0800

    new

--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -7,9 +7,15 @@ export default function Navbar() {
     return (
         <nav className={styles.nav}>
             <div className={`container ${styles.container}`}>
-                <Link href="/" className={styles.logo}>
-                    Glass Market
-                </Link>
+                <div className={styles.brand}>
+                    <Link href="/" className={styles.logo}>
+                        Glass Market
+                    </Link>
+                    <a href="https://discord.gg/Hg8qTytv5K" target="_blank" rel="noopener noreferrer" className={styles.discordBtn}>
+                        <img src="/assets/icons/contact/discord.svg" alt="Discord" className={styles.discordIcon} />
+                        Join
+                    </a>
+                </div>
                 <div className={styles.links}>
                     <Link href="/about">About</Link>
                     <Link href="/list-account">List Account</Link>
