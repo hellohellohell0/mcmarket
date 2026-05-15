427c312a820f546596f645064dcb330956952591
hellohellohell012321 <hhellohellohell012321@gmail.com>
2026-05-15 13:28:17 -0700

    Remove Neon adapter and use Prisma native engine

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool, neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import ws from 'ws'
-
-neonConfig.webSocketConstructor = ws
 
 const prismaClientSingleton = () => {
-    // We expect DATABASE_URL to be present in env
-    const connectionString = process.env.DATABASE_URL
-    const pool = new Pool({ connectionString })
-    // @ts-ignore - The types of Pool from neondatabase/serverless might mismatch what Prisma expects
-    const adapter = new PrismaNeon(pool)
-    return new PrismaClient({ adapter })
+    return new PrismaClient()
 }
 
 declare global {
